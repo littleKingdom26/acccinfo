25c2f4676fd615b406d2480d827e00df0f83c4e4
taeho <wingindream@gmail.com>
2020-03-18 22:08:26 +0900

    2020.03.18 22:08 권한및 기존 게시물 등록 페이지 삭제

--- src/main/java/info/team23h/acc/controller/BbsController.java
+++ src/main/java/info/team23h/acc/controller/BbsController.java
@@ -57,30 +57,6 @@ public class BbsController {
 	}
 
 	/**
-	 * 글 작성
-	 *
-	 * @param model      the model
-	 * @param bbsNameSeq the bbs name seq
-	 * @param bbsSearch  the bbs search
-	 * @param check      the check
-	 * @return the string
-	 */
-	@GetMapping("/bbs/{bbsNameSeq}/write/{check}")
-	public String createBbs(Model model
-							, @PathVariable("bbsNameSeq") long bbsNameSeq
-							, @ModelAttribute("bbsSearch") BbsSearch bbsSearch
-							, @PathVariable("check") String check){
-		bbsSearch.setNameSeq(bbsNameSeq);
-		if(bbsNameSeq != write){
-			if(!writeCheck.equals(check)){
-				return "error/404";
-			}
-		}
-		model.addAttribute("bbsNameSeq", bbsNameSeq);
-		return "bbs/write";
-	}
-
-	/**
 	 * 글 상세 보기
 	 *
 	 * @param model      the model
@@ -101,34 +77,7 @@ public class BbsController {
 		return "bbs/view";
 	}
 
-	/**
-	 * 글 수정
-	 *
-	 * @param model      the model
-	 * @param bbsNameSeq the bbs name seq
-	 * @param seq        the seq
-	 * @param check      the check
-	 * @param bbsSearch  the bbs search
-	 * @return the string
-	 */
-	@GetMapping("/bbs/{bbsNameSeq}/{seq}/update/{check}")
-	public String createBbs(Model model,
-							@PathVariable("bbsNameSeq") long bbsNameSeq,
-							@PathVariable("seq") long seq,
-							@PathVariable("check") String check,
-							@ModelAttribute("bbsSearch") BbsSearch bbsSearch) {
-		bbsSearch.setNameSeq(bbsNameSeq);
-		bbsSearch.setBbsSeq(seq);
-		if(bbsNameSeq != write){
-			if(!writeCheck.equals(check)){
-				return "error/404";
-			}
-		}
-		model.addAttribute("data", bbsService.loadBbsView(bbsSearch));
-		model.addAttribute("bbsNameSeq", bbsNameSeq);
-		model.addAttribute("bbsSeq", seq);
-		return "bbs/update";
-	}
+
 
 
 
