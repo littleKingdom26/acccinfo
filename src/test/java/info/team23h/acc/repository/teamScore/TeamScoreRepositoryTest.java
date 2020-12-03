d9caef3f7394d8a55c53e9c770561840f6aec236
taeho <wingindream@gmail.com>
2020-12-04 00:53:38 +0900

    테스트 커밋

---
+++ src/test/java/info/team23h/acc/repository/teamScore/TeamScoreRepositoryTest.java
@@ -0,0 +1,48 @@
+package info.team23h.acc.repository.teamScore;
+
+import info.team23h.acc.service.teamScore.TeamScoreService;
+import info.team23h.acc.vo.team.TeamScoreSearchVO;
+import info.team23h.acc.vo.team.TeamScoreTeamInfoResultVO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class TeamScoreRepositoryTest {
+
+	@Autowired
+	TeamScoreRepository teamScoreRepository;
+
+	@Autowired
+	TeamScoreService teamScoreService;
+
+	@Test
+	public void test(){
+
+		TeamScoreSearchVO teamScoreSearchVO = new TeamScoreSearchVO();
+		teamScoreSearchVO.setSearchEventDt("2020-11");
+		List<TeamScoreTeamInfoResultVO> teamScore = teamScoreRepository.findTeamScore(teamScoreSearchVO);
+
+
+	}
+
+	@Test
+	public void 리스트조회_test() {
+
+		List<TeamScoreSearchVO> allEventDtGroupBy = teamScoreRepository.findAllEventDtGroupBy();
+
+	}
+
+	@Test
+	public void 팀_디테일_조회(){
+		TeamScoreSearchVO teamScoreSearchVO = new TeamScoreSearchVO();
+		//teamScoreSearchVO.setSearchEventDt("2020-10");
+		teamScoreSearchVO.setTeamInfoSeq(5L);
+
+		teamScoreService.findTeamScoreDetail(teamScoreSearchVO);
+
+	}
+
+}
