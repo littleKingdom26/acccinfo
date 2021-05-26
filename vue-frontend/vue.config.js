2fe38b5a4d0ed469975708f97bd5dbf76124c07d
Kabkee <kabkee.moon@gmail.com>
2021-05-27 01:10:06 +0900

    1. 누락 기본 views 폴더 추가 2. vue.config.js build asset 경로 변경 3. test build

--- vue-frontend/vue.config.js
+++ vue-frontend/vue.config.js
@@ -8,5 +8,22 @@ module.exports = {
 		const svgRule = config.module.rule("svg");
 		svgRule.uses.clear();
 		svgRule.use("vue-svg-loader").loader("vue-svg-loader");
+
+		config.module
+			.rule('images')
+			.use('url-loader')
+			.tap(options => Object.assign({}, options, { name: 'vue_assets/img/[name].[hash:8].[ext]' }));
+	},
+	css: {
+		extract: {
+			filename: 'vue_assets/css/[name].[hash:8].css',
+			chunkFilename: 'vue_assets/css/[name].[hash:8].css',
+		},
+	},
+	configureWebpack: {
+		output: {
+			filename: 'vue_assets/js/[name].[hash:8].js',
+			chunkFilename: 'vue_assets/js/[name].[hash:8].js',
+		}
 	}
 };
